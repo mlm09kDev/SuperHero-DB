19d05822e02df9048fecc72436c6f441f9754cc2
Manuel <mlm09kdev@gmail.com>
2020-02-25 18:18:05 -0500

    updated project Structure

--- app/src/androidTest/java/com/mlm09kdev/superHeroDB/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mlm09kdev/superHeroDB/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mlm09kdev.quickweather", appContext.packageName)
+        assertEquals("com.mlm09kdev.SuperHeroDB", appContext.packageName)
     }
 }
